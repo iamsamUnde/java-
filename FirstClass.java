c47e1d59bb296edb1f12fca4aa28e2cfc52ce7a3
kamal77sum <kamal.sumfactor@gmail.com>
2023-06-03 15:28:56 +0530

    commit

---
+++ FirstClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class FirstClass{
+    public static void main(String[] args) {
+        //input
+        Scanner sc= new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }   
+} 
