2f60471930ed1fef706611274ee62d7a18b05b4b
kamal77sum <kamal.sumfactor@gmail.com>
2023-06-03 15:56:16 +0530

    adding two numbers

--- FirstClass.java
+++ FirstClass.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 class FirstClass{
     public static void main(String[] args) {
-        //input
+        //add two numbers
         Scanner sc= new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(name);
+        System.out.println("Enter two number u are adding > ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c= a+b;
+        System.out.println("result="+c);
     }   
 } 
